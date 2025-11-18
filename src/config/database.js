028d0da2c01315f810b9efbcac11988b52336035
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2025-11-18 21:55:17 +0530

    M

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,20 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI);
+//     console.log("MongoDB connected successfully.");
+//   } catch (error) {
+//     console.error("MongoDB connection failed:", error.message);
+//     // Exit process with failure
+//     process.exit(1);
+//   }
+// };
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("MongoDB connected successfully.");
-  } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
-    // Exit process with failure
-    process.exit(1);
-  }
+  await mongoose.connect(
+    "mongodb+srv://whoisvipul25:qjZbL3EsVfsv4mHz@learningnode.op27kqd.mongodb.net/devTinder"
+  );
 };
 
 module.exports = connectDB;
