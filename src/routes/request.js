5df698cade7259bd25ad7924f0875d3d34de710e
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2025-11-20 19:06:52 +0530

    added email config

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,6 +1,8 @@
 // routes/requestRouter.js
 const express = require("express");
 const requestRouter = express.Router();
+const dotenv = require("dotenv");
+dotenv.config();
 
 const { UserAuth } = require("../middlewares/userAuth.js"); // middleware that attaches req.user
 const { User } = require("../models/user.js");
@@ -52,6 +54,12 @@ requestRouter.post(
       });
 
       const data = await newRequest.save();
+
+      const emailRes = await sendEmail.run(
+        "A new friend request from " + req.user.firstName,
+        req.user.firstName + " is " + status + " in " + toUser.firstName
+      );
+      console.log(emailRes);
 
       res.json({
         message: `${req.user.firstName} is ${status} in ${toUser.firstName}`,
